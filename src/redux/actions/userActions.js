5491b6962c44e0559fd12c22cba643e4a5be7ad1
abisheik88 <abishekvijayarangan@gmail.com>
2023-02-02 07:52:54 +0530

    route change

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -26,7 +26,7 @@ export const userRegister = (reqObj) => async dispatch => {
     dispatch({ type: 'LOADING', payload: true })
 
     try {
-        const response = await axios.post('/api/users/register', reqObj)
+        const response = await axios.post('https://webcode-2.onrender.com/api/users/register', reqObj)
         message.success('Registration successfull')
         setTimeout(() => {
             window.location.href = '/login'
